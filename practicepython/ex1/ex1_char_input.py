938357445ed4ad1be3a7f70d072d3a0b91d4c278
drp <rtotop@gmail.com>
2018-07-09 23:15:13 -0700

    update ex1 for any date:  extra 1, extra 2

--- practicepython/ex1/ex1_char_input.py
+++ practicepython/ex1/ex1_char_input.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-"""Example 1 from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html"""
+"""Example 1 from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+==========================
+Prompts user for name, age, and number of times to print. 
+Prints year until user will turn age 100 in a custom message tailored to the user.
+"""
+
+import datetime
 
 def main():
+    now = datetime.datetime.now()
+
     name = input("Please enter your name\n")
     age = int(input("Please enter your age, in years\n"))
+    multiplier = int(input("Please enter the number of times to print\n"))
 
-    # assuming current year is 2018
-    yearWillTurn100 = 2018 + (100-age)
+    yearWillTurn100 = now.year + (100-age)
+    
+    outputMsg =  "Hello {}, you will be 100 in the year: {}\n".format(name, yearWillTurn100)
 
-    print("You will be 100 in the year:", yearWillTurn100)
+    print(multiplier*outputMsg)
 
 if __name__=="__main__":
     main()
